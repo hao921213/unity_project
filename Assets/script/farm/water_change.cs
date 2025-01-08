729e111277df5795ec3d101cd7aaf2d6f58de21f
hao921213 <113426712+hao921213@users.noreply.github.com>
2025-01-08 10:51:25 +0800

    沒水就死光

--- Assets/script/farm/water_change.cs
+++ Assets/script/farm/water_change.cs
@@ -1,3 +1,4 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class water_change : MonoBehaviour
@@ -10,80 +11,50 @@ public class water_change : MonoBehaviour
     public bag farm6;
     public time1 time;
     public water_controller all_water;
-    private int[] change=new int[6];
+    public item none;
+    public farm_controller farm_1;
+    public farm_controller farm_2;
+    public farm_controller farm_3;
+    public farm_controller farm_4;
+    public farm_controller farm_5;
+    public farm_controller farm_6;
+    private int change=0;
+    private bag[] farms=new bag[6];
+    private farm_controller[] farms_=new farm_controller[6];
+    
 
+    private void Start()
+    {
+        farms[0]=farm1;farms_[0]=farm_1;
+        farms[1]=farm2;farms_[1]=farm_2;
+        farms[2]=farm3;farms_[2]=farm_3;
+        farms[3]=farm4;farms_[3]=farm_4;
+        farms[4]=farm5;farms_[4]=farm_5;
+        farms[5]=farm6;farms_[5]=farm_6;
+    }
     private void Update()
     {
-        if(time.count>=6 && time.count<19 && change[0]==0){
-            for(int i=0;i<farm1.itemlist.Count;i++){
-                if(farm1.itemlist[i].item_name!="null"){
-                    all_water.waters[0]-=(float)farm1.itemlist[i].water;
+        if(time.count >= 6 && time.count<19 && change==0){
+            for(int i=0;i<6;i++){
+                if(all_water.waters[i]<=0){
+                    for(int j=0;j<farms[i].itemlist.Count;j++){
+                        if(farms[i].itemlist[j].item_name!="null"){
+                            farms[i].itemlist[j]=none;
+                        }
+                    }
+                    farms_[i].update_animal();
+                    continue;
+                }
+                for(int j=0;j<farms[i].itemlist.Count;j++){
+                    if(farms[i].itemlist[j].item_name!="null"){
+                        all_water.waters[i]-=(float)farms[j].itemlist[j].water;
+                    }
                 }
             }
-            change[0]=1;
+            change=1;
         }
-        else if(time.count>20 && change[0]==1){
-            change[0]=0;
-        }
-
-                if(time.count>=6 && time.count<19 && change[1]==0){
-            for(int i=0;i<farm2.itemlist.Count;i++){
-                if(farm2.itemlist[i].item_name!="null"){
-                    all_water.waters[2]-=(float)farm2.itemlist[i].water;
-                }
-            }
-            change[1]=1;
-        }
-        else if(time.count>20 && change[1]==1){
-            change[1]=0;
-        }
-
-                if(time.count>=6 && time.count<19 && change[2]==0){
-            for(int i=0;i<farm3.itemlist.Count;i++){
-                if(farm3.itemlist[i].item_name!="null"){
-                    all_water.waters[3]-=(float)farm3.itemlist[i].water;
-                }
-            }
-            change[2]=1;
-        }
-        else if(time.count>20 && change[2]==1){
-            change[2]=0;
-        }
-
-                if(time.count>=6 && time.count<19 && change[3]==0){
-            for(int i=0;i<farm4.itemlist.Count;i++){
-                if(farm4.itemlist[i].item_name!="null"){
-                    all_water.waters[3]-=(float)farm4.itemlist[i].water;
-                }
-            }
-            change[3]=1;
-        }
-        else if(time.count>20 && change[3]==1){
-            change[3]=0;
-        }
-
-                if(time.count>=6 && time.count<19 && change[4]==0){
-            for(int i=0;i<farm5.itemlist.Count;i++){
-                if(farm5.itemlist[i].item_name!="null"){
-                    all_water.waters[4]-=(float)farm5.itemlist[i].water;
-                }
-            }
-            change[4]=1;
-        }
-        else if(time.count>20 && change[4]==1){
-            change[4]=0;
-        }
-
-                if(time.count>=6 && time.count<19 && change[5]==0){
-            for(int i=0;i<farm6.itemlist.Count;i++){
-                if(farm6.itemlist[i].item_name!="null"){
-                    all_water.waters[5]-=(float)farm6.itemlist[i].water;
-                }
-            }
-            change[5]=1;
-        }
-        else if(time.count>20 && change[5]==1){
-            change[5]=0;
+        else if(time.count>20 && change==1){
+            change=0;
         }
     }
 }
