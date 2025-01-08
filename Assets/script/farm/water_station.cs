9bfe0ed1808027182728dd2f9e3eef1f367ec65f
hao921213 <113426712+hao921213@users.noreply.github.com>
2025-01-08 11:30:36 +0800

    可以補水

---
+++ Assets/script/farm/water_station.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class water_station : MonoBehaviour
+{
+    public GameObject station;
+    public hand_take hand;
+    public water_controller water;
+    public int index;
+    private int player_in;
+
+    private void Update()
+    {
+        if(player_in==1 && hand.item.item_name=="waterer" && Input.GetKeyDown(KeyCode.E)){
+            water.waters[index]=50;
+            Debug.Log("補水成功");
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player")) {
+            player_in=1;
+        }  
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.CompareTag("Player")) {
+            player_in=0;
+        }  
+    }
+
+}
