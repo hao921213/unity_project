2077ba297ef9a80662ac4fb15394ac2d870fc4dc
hao921213 <113426712+hao921213@users.noreply.github.com>
2025-01-10 07:48:37 +0800

    1245789

---
+++ Assets/script/enemies/7day_monster.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using System.Collections;
+using System.Threading;
+
+
+public class day7monster : MonoBehaviour
+{
+   public GameObject[] monsterPrefabs; // 不同種類的怪物
+    public Vector2 mapBottomLeft;
+    public Vector2 mapTopRight;
+    public float spawnInterval = 2f;   // 生成間隔
+    public int maxMonsters = 10;       // 最大生成數量
+
+    public time1 time1;
+
+    private int currentMonsterCount = 0; // 當前已生成的怪物數量
+
+    void Start()
+    {
+        
+         
+    }
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        if(time1.day % 7 == 0)
+        {
+            StartCoroutine(SpawnMonstersOverTime());  
+        }
+    }
+
+    IEnumerator SpawnMonstersOverTime()
+    {
+        while (currentMonsterCount < maxMonsters)
+        {
+            float randomX = Random.Range(mapBottomLeft.x, mapTopRight.x);
+            float randomY = Random.Range(mapBottomLeft.y, mapTopRight.y);
+            Vector2 spawnPosition = new Vector2(randomX, randomY);
+
+            // 隨機選擇怪物
+            int monsterIndex = Random.Range(0, monsterPrefabs.Length);
+            Instantiate(monsterPrefabs[monsterIndex], spawnPosition, Quaternion.identity);
+
+            currentMonsterCount++; // 增加當前怪物數量
+
+            yield return new WaitForSeconds(spawnInterval); // 等待指定間隔再生成
+        }
+    }
+}
---
+++ Assets/script/enemies/enemy_data.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "enemy_data", menuName = "Scriptable Objects/enemy_data")]
+public class enemy_data : ScriptableObject
+{
+     public GameObject enemy;
+    public item item;
+}
