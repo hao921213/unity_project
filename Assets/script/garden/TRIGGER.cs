c343a89e06ecae6b445120fd66bbe8c6817e5f82
hao921213 <113426712+hao921213@users.noreply.github.com>
2025-01-10 04:20:20 +0800

    花園100

--- Assets/script/garden/TRIGGER.cs
+++ Assets/script/garden/TRIGGER.cs
@@ -1,29 +1,71 @@
 using System.Collections.Generic;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class TRIGGER : MonoBehaviour
 {
+    public int number;
     private int enter = 0;
-    private Sprite newSprite;
     private SpriteRenderer spriteRenderer;
     public hand_take hand;
     private int find = -1;
     public time1 time1;
+    public cropstate state;
     public crop_pic_class carrot;
     public crop_pic_class green_pepper;
     public crop_pic_class pump;
     public crop_pic_class straw;
     public crop_pic_class w_carrot;
     public crop_pic_class watermelon;
+    public Animator animator;
+    private int time;
     private int time_chack;
-    private int phrase = 0;
+    private int phrase = -1;
     private List<string> crop_type = new List<string>{"carrot_seed","green_pepper_seed","pump_seed","straw_seed"
     ,"watermelon_seed","w_carrot_seed"};
     public bag player_bag;
+    private int watered = 0;
+    private int stop_hour;
+    private int stop_day;
+    private int pass_hour;
+
+    void time_pass_calculate()
+    {
+        if (phrase != -1 && watered ==1 && stop_hour != time1.hour)
+        {
+            if(stop_day == time1.day){
+                pass_hour = time1.hour - stop_hour;
+                phrase = phrase + pass_hour;
+                save_phase(phrase);
+            }
+            else{
+                pass_hour = 24 - stop_hour + 24 * (time1.day - stop_day -1) + time1.hour;
+                phrase = phrase + pass_hour;
+                save_phase(phrase);
+            }
+        }
+    }
     private void Start()
     {
-
+        phrase = state.crop_phase[number-1];
+        find = state.crop_type[number-1];
+        watered = state.crop_watered[number-1];
+        time_pass_calculate();
+        Debug.Log("phrase: " + phrase);
+        Debug.Log("find: " + find);
+    }
+    void after_used(item item)
+    {
+        for(int i=0;i<player_bag.itemlist.Count;i++){
+            if(player_bag.itemlist[i]==item){
+                player_bag.itemlist[i].held--;
+                bag_controller.RefreshItem();
+                return;
+            }
+        }
+        return;
     }
     void player_get(item item){
         Debug.Log(item);
@@ -75,227 +117,162 @@ public class TRIGGER : MonoBehaviour
             {
                 find = i;
                 time_chack = time1.hour;
-                //Debug.Log("1111time_chack: " + time_chack);
+                save_type(find);
             }
-            //Debug.Log("2222time_chack: " + time_chack);
         }
+    }
+    
+    private void save_phase(int a)
+    {
+        state.crop_phase[number-1] = a;
+    }
+    private void save_type(int a)
+    {
+        state.crop_type[number-1] = a;
+    }
+    private void save_water(int a)
+    {
+        state.crop_watered[number-1] = a;
+    }
+    void phrase_switch(crop_pic_class crop)
+    {
+        //Debug.Log("phrase: " + phrase);
+        if(phrase == 0)
+        {
+            spriteRenderer.sprite = crop.sprite0;
+            //Debug.Log("find: " + find);
+        }
+        else if(phrase == 1)
+        {
+            spriteRenderer.sprite = crop.sprite1;
+            //Debug.Log("0");
+        }
+        else if(phrase == 2)
+        {
+            spriteRenderer.sprite = crop.sprite2;
+            //Debug.Log("0");
+        }
+        else if(phrase == 3)
+        {
+            spriteRenderer.sprite = crop.sprite3;
+            //Debug.Log("0");
+        }
+        else if(phrase >= 4)
+        {
+            spriteRenderer.sprite = crop.sprite4;
+            if (enter == 1 && Input.GetKeyDown(KeyCode.E))
+            {
+                spriteRenderer.sprite = null;
+                player_get(crop.item);
+                find = -1;
+                phrase = -1;
+                watered = 0;
+                save_phase(phrase);
+                save_type(find);
+                save_water(0);
+                //Debug.Log("0");
+            }
+        }
+    }
+    void water()
+    {
+        animator.SetInteger("onwater",1);
+        watered = 1;
+        save_water(1);
     }
     private void Update()
     {
-        if (enter == 1 && Input.GetKeyDown(KeyCode.E) && phrase == 0)
+        if (enter == 1 && Input.GetKeyDown(KeyCode.E) && phrase == -1)
         {
             if (hand.item != null && hand.item.tag == "crop")
             {
                 update_image();
+                after_used(hand.item);
+                hand.item = null;
+                phrase = 0;
+                save_phase(phrase);
                 //Debug.Log("find: " + find + " time_chack: " + time_chack + " time1_hour: " + time1.hour);
             }
         }
+        if (enter == 1 && Input.GetKeyDown(KeyCode.E) && phrase == 0)
+        {
+            if (hand.item != null && hand.item.item_name == "waterer")
+            {
+                water();
+                //Debug.Log("find: " + find + " time_chack: " + time_chack + " time1_hour: " + time1.hour);
+            }
+        }
+        
         spriteRenderer = GetComponent<SpriteRenderer>();
         
-        if (find != -1 && time_chack != time1.hour)
+        if (find != -1 && time_chack != time1.hour && watered == 1) //
         {
             phrase ++;
+            save_phase(phrase);
             time_chack = time1.hour;
-            Debug.Log("phrase: " + phrase);
+            //Debug.Log("phrase: " + phrase);
         }
         switch (find)
         {
             case 0:
-                if(phrase == 0)
-                {
-                    spriteRenderer.sprite = carrot.sprite0;
-                    Debug.Log("find: " + find);
-                }
-                else if(phrase == 1)
-                {
-                    spriteRenderer.sprite = carrot.sprite1;
-                    //Debug.Log("0");
-                }
-                else if(phrase == 2)
-                {
-                    spriteRenderer.sprite = carrot.sprite2;
-                    //Debug.Log("0");
-                }
-                else if(phrase == 3)
-                {
-                    spriteRenderer.sprite = carrot.sprite3;
-                    //Debug.Log("0");
-                }
-                else if(phrase >= 4)
-                {
-                    spriteRenderer.sprite = carrot.sprite4;
-                    if (enter == 1 && Input.GetKeyDown(KeyCode.E))
-                    {
-                        spriteRenderer.sprite = null;
-                        player_get(carrot.item);
-                        find = -1;
-                        phrase = 0;
-                        //Debug.Log("0");
-                    }
-                }
+                phrase_switch(carrot);
                 break;
             case 1:
-                if(phrase == 0)
-                {
-                    spriteRenderer.sprite = green_pepper.sprite0;
-                }
-                else if(phrase == 1)
-                {
-                    spriteRenderer.sprite = green_pepper.sprite1;
-                }
-                else if(phrase == 2)
-                {
-                    spriteRenderer.sprite = green_pepper.sprite2;
-                }
-                else if(phrase == 3)
-                {
-                    spriteRenderer.sprite = green_pepper.sprite3;
-                }
-                else if(phrase >= 4)
-                {
-                    spriteRenderer.sprite = green_pepper.sprite4;
-                    if (enter == 1 && Input.GetKeyDown(KeyCode.E))
-                    {
-                        spriteRenderer.sprite = null;
-                        player_get(green_pepper.item);
-                        find = -1;
-                        phrase = 0;
-                    }
-                }
+                phrase_switch(green_pepper);
                 break;
             case 2:
-
-                if(phrase == 0)
-                {
-                    spriteRenderer.sprite = pump.sprite0;
-                }
-                else if(phrase == 1)
-                {
-                    spriteRenderer.sprite = pump.sprite1;
-                }
-                else if(phrase == 2)
-                {
-                    spriteRenderer.sprite = pump.sprite2;
-                }
-                else if(phrase == 3)
-                {
-                    spriteRenderer.sprite = pump.sprite3;
-                }
-                else if(phrase >= 4)
-                {
-                    spriteRenderer.sprite = pump.sprite4;
-                    if (enter == 1 && Input.GetKeyDown(KeyCode.E))
-                    {
-                        spriteRenderer.sprite = null;
-                        player_get(pump.item);
-                        
-                        find = -1;
-                        phrase = 0;
-                    }
-                }
+                phrase_switch(pump);
                 break;
             case 3:
-
-                if(phrase == 0)
-                {
-                    spriteRenderer.sprite = straw.sprite0;
-                }
-                // else if(phrase == 1)
-                // {
-                //     spriteRenderer.sprite = straw.sprite1;
-                // }
-                // else if(phrase == 2)
-                // {
-                //     spriteRenderer.sprite = straw.sprite2;
-                // }
-                // else if(phrase == 3)
-                // {
-                //     spriteRenderer.sprite = straw.sprite3;
-                // }
-                else if(phrase >= 1)
-                {
-                    spriteRenderer.sprite = straw.sprite4;
-                    
-                    if (enter == 1 && Input.GetKeyDown(KeyCode.E))
-                    {
-                        Debug.Log("hi");
-                        spriteRenderer.sprite = null;
-                        Debug.Log(player_bag.itemlist.Count);
-                        player_get(straw.item);
-                        find = -1;
-                        phrase = 0;
-                    }
-                    
-                }
+                phrase_switch(straw);
                 break;
             case 4:
-
-                if(phrase == 0)
-                {
-                    spriteRenderer.sprite = watermelon.sprite0;
-                }
-                else if(phrase == 1)
-                {
-                    spriteRenderer.sprite = watermelon.sprite1;
-                }
-                else if(phrase == 2)
-                {
-                    spriteRenderer.sprite = watermelon.sprite2;
-                }
-                else if(phrase == 3)
-                {
-                    spriteRenderer.sprite = watermelon.sprite3;
-                }
-                else if(phrase >= 4)
-                {
-                    spriteRenderer.sprite = watermelon.sprite4;
-                    if (enter == 1 && Input.GetKeyDown(KeyCode.E))
-                    {
-                        spriteRenderer.sprite = null;
-                        player_get(watermelon.item);
-                        
-                        find = -1;
-                        phrase = 0;
-                    }
-                    
-                }
+                phrase_switch(watermelon);
                 break;
             case 5:
-                if(phrase == 0)
-                {
-                    spriteRenderer.sprite = w_carrot.sprite0;
-                }
-                else if(phrase == 1)
-                {
-                    spriteRenderer.sprite = w_carrot.sprite1;
-                }
-                else if(phrase == 2)
-                {
-                    spriteRenderer.sprite = w_carrot.sprite2;
-                }
-                else if(phrase == 3)
-                {
-                    spriteRenderer.sprite = w_carrot.sprite3;
-                }
-                else if(phrase >= 4)
-                {
-                    spriteRenderer.sprite = w_carrot.sprite4;
-                    if (enter == 1 && Input.GetKeyDown(KeyCode.E))
-                    {
-                        spriteRenderer.sprite = null;
-                        player_get(w_carrot.item);
-                        
-                        find = -1;
-                        phrase = 0;
-                    }
-                }
+                phrase_switch(w_carrot);
                 break;
             default:
                 // 當表達式的值不匹配任何 case 時執行此處代碼
                 break;
         }
+        stop_hour = time1.hour;
+        state.stop_hour[number-1] = stop_hour;
+        stop_day = time1.day;
+        state.stop_day[number-1] = stop_day;
         
     }
 
-
 }
+// if(phrase == 0)
+// {
+//     spriteRenderer.sprite = straw.sprite0;
+// }
+// else if(phrase == 1)
+// {
+//     spriteRenderer.sprite = straw.sprite1;
+// }
+// else if(phrase == 2)
+// {
+//     spriteRenderer.sprite = straw.sprite2;
+// }
+// else if(phrase == 3)
+// {
+//     spriteRenderer.sprite = straw.sprite3;
+// }
+// else if(phrase >= 1)
+// {
+//     spriteRenderer.sprite = straw.sprite4;
+    
+//     if (enter == 1 && Input.GetKeyDown(KeyCode.E))
+//     {
+//         //Debug.Log("hi");
+//         spriteRenderer.sprite = null;
+//         //Debug.Log(player_bag.itemlist.Count);
+//         player_get(straw.item);
+//         find = -1;
+//         phrase = -1;
+//         watered = 0;
+//     }
+    
+// }
--- Assets/script/garden/cropstate.cs
+++ Assets/script/garden/cropstate.cs
@@ -1,14 +1,20 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.Linq;
 [CreateAssetMenu(fileName = "crop", menuName = "game/crop")]
 
 public class cropstate : ScriptableObject
 {
-    public List<string> crop_type = new List<string>();
-    public List<int> crop_time = new List<int>();
+    public List<int> crop_type = new List<int>();
+    public List<int> crop_phase = new List<int>();
+    public List<int> crop_watered = new List<int>();
+    public List<int> stop_hour = new List<int>();
+    public List<int> stop_day = new List<int>();
+
 
 }
+
 
 
 
